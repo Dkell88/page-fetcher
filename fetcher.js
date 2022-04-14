ff902a9a156c976d0224257f2817605786f89ea8
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-13 21:24:02 -0700

    Adding initial version of website fetcher, no stretch

---
+++ fetcher.js
@@ -0,0 +1,28 @@
+
+console.log(`\n\n\n**************************************************`);
+
+
+
+const URL = process.argv[2];
+const localPath = process.argv[3];
+
+const request = require('request');
+const fs = require('fs');
+
+console.log(`URL: ${URL} - ${typeof(URL)}  localPath: ${localPath} - ${typeof(localPath)}`);
+
+console.log('request');
+request(URL, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the
+  fs.writeFile(localPath, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.error("File created ... I think");
+    console.error(`File size: ${body.length}bytes`);
+  });
+
+});
+
